71149c2f42dc5cf27d68e7cefa347ea4f422cfcb
root <adivyoseph@yahoo.com>
2022-03-01 17:40:33 +0000

    support smt 8

--- topology.c
+++ topology.c
@@ -19,8 +19,8 @@
 
 #include "topology.h"
 
-int init(void);
-int display(void);
+static int init(void);
+static int display(void);
 
 topology_t C_topology = {
 
@@ -29,10 +29,14 @@ topology_t C_topology = {
 
 };
 
-// private functions
-int f_getNumCores(void);
-int f_getCoreData(int i_core);
-
+// private helper functions
+static int f_getNumCores(void);
+static int f_getCoreData(int i_core);
+static core_t   *f_getPCore(int i_core);
+static llc_t    *f_getLlc(int i_llc);
+static node_t   *f_getNode(int i_node);
+static int       f_setNode(core_t *p_core, llc_t *p_llc);
+static int       f_setLlc(core_t   *p_core);
 
 /**
  * @brief initialise topology from proc and sysfs
@@ -41,26 +45,19 @@ int f_getCoreData(int i_core);
  * 
  * @param void 
  * 
- * @return int 
+ * @return int not used
  */
-int init(void){
-    int i_core;
-
-
-    //determine n_sockets
+static int init(void){
+    int i_core;             //local iterator variable
+    //determine the number cores
+    //sysfs topology uses core as the primary index
     C_topology.i_cores = f_getNumCores();
-    //populate topology
-
+    //populate topology structure
     C_topology.p_core_head = NULL;
     C_topology.p_core_tail = NULL;
     for (i_core = 0 ;i_core < C_topology.i_cores; i_core++) {
         f_getCoreData(i_core);
     }
-
-
-
-
-    printf("Init done\n");
     return 0;
 }
 
@@ -71,18 +68,14 @@ int init(void){
  * 
  * @param void 
  * 
- * @return int 
+ * @return int not used
  */
 int display(void){
 
     int i, j;
 
     core_t   *p_core = C_topology.p_core_head;
-
     node_t   *p_node = C_topology.p_node_head;
-
-
-    //llc_t   *p_llc; /// = C_topology.p_llc_head;
 
     printf("Display walk cores\n");
     while (p_core) {
@@ -103,8 +96,6 @@ int display(void){
                 printf("\t\t%s\n", ((core_t *)p_node->p_llcs[i]->vp_cores[j])->c_name);
             }
         }
-
-
         p_node = p_node->p_next;
     }
 
@@ -112,8 +103,14 @@ int display(void){
 }
 
 
-
-int f_getNumCores(void){
+/**
+ * @brief use sysfs to get core count
+ * 
+ * @author root (1 Mar 2022)
+ * 
+ * @return int number of cores (including SMT)
+ */
+static int f_getNumCores(void){
     int rc = 0;
     char *p_path = "/sys/devices/system/cpu/possible";
     FILE *fp;
@@ -138,13 +135,20 @@ int f_getNumCores(void){
     return rc+1;
 }
 
-core_t   *f_getPCore(int i_core){
-
+/**
+ * @brief locate core object instance by core index
+ * 
+ * @author root (1 Mar 2022)
+ * 
+ * @param i_core 
+ * 
+ * @return core_t* pointer to core object or NULL if not found
+ */
+static core_t   *f_getPCore(int i_core){
     core_t   *p_core = C_topology.p_core_head;
 
     while (p_core) {
         if (p_core->i_gIndex == i_core) {
-            //printf("f_getPCore(%d) found\n", i_core);
             break;
         }
         p_core = p_core->p_next;
@@ -153,14 +157,20 @@ core_t   *f_getPCore(int i_core){
     return p_core;
 }
 
-
-llc_t   *f_getLlc(int i_llc){
-
+/**
+ * @brief loacate LLC (Late level cache) by index
+ * 
+ * @author root (1 Mar 2022)
+ * 
+ * @param i_llc 
+ * 
+ * @return llc_t* llc object or NULL if not found
+ */
+static llc_t   *f_getLlc(int i_llc){
     llc_t   *p_llc = C_topology.p_llc_head;
 
     while (p_llc) {
         if (p_llc->i_gIndex == i_llc) {
-            //printf("f_getPCore(%d) found\n", i_core);
             break;
         }
         p_llc = p_llc->p_next;
@@ -169,14 +179,20 @@ llc_t   *f_getLlc(int i_llc){
     return p_llc;
 }
 
-
-node_t   *f_getNode(int i_node){
-
+/**
+ * @brief locate node object instance by index
+ * 
+ * @author root (1 Mar 2022)
+ * 
+ * @param i_node 
+ * 
+ * @return node_t* node object or NULL if not found
+ */
+static node_t   *f_getNode(int i_node){
     node_t   *p_node = C_topology.p_node_head;
 
     while (p_node) {
         if (p_node->i_gIndex == i_node) {
-            //printf("f_getPCore(%d) found\n", i_core);
             break;
         }
         p_node = p_node->p_next;
@@ -185,8 +201,20 @@ node_t   *f_getNode(int i_node){
     return p_node;
 }
 
-
-int f_setNode(core_t *p_core, llc_t *p_llc){
+/**
+ * @brief link llc to parent node and create it if it does not 
+ *        exist. Create back pointer from node to child llc.
+ *        called when a new llc object is created 
+ * 
+ * @author root (1 Mar 2022)
+ * 
+ * @param p_core contains core global index, used to look up 
+ *               parent node index
+ * @param p_llc llc to associate with node
+ * 
+ * @return int not used
+ */
+static int f_setNode(core_t *p_core, llc_t *p_llc){
     char s_path[128]; 
     int n;
     DIR *p_dir;
@@ -203,11 +231,12 @@ int f_setNode(core_t *p_core, llc_t *p_llc){
             if (!strncmp(p_dirEntry->d_name, "node", 4)) {
                 //found node
                 n = atoi(&p_dirEntry->d_name[4]);
-                printf("\tnode %s %d\n", p_dirEntry->d_name, n);
+                //printf("\tnode %s %d\n", p_dirEntry->d_name, n);
 
-                p_node = f_getNode(n);
+                p_node = f_getNode(n);  //see if node already exits
 
                 if (p_node == NULL) {
+                    //first time create node
                     p_node = (node_t *) malloc(sizeof(node_t));
                     C_topology.i_nodes++;
                     if (C_topology.p_node_head == NULL) {
@@ -219,10 +248,11 @@ int f_setNode(core_t *p_core, llc_t *p_llc){
                     C_topology.p_node_tail = p_node;
                     p_node->p_next = NULL;
                     p_node->i_gIndex = n;
-                    sprintf(p_node->c_name, "node%d", n);
-
+                    sprintf(p_node->c_name, "node%d", n);  //used for debug and display only
                 }
-                p_llc->vp_node = p_node;
+                p_llc->vp_node = p_node;  //set llc parent node
+                //sysfs enumeration is in order so the following is a safe assumption
+                //add child pointer for this llc
                 p_node->p_llcs[p_node->i_llc_n] = p_llc;
                 p_node->i_llc_n++;
                 break;
@@ -235,16 +265,23 @@ int f_setNode(core_t *p_core, llc_t *p_llc){
     return 0;
 }
 
-int f_setLlc(core_t   *p_core){
-
-//    int rc = 0;
-
+/**
+ * @brief get core llc index (global id) 
+ *        create if required, also adds node if required
+ *        create llc to core back linkages 
+ * 
+ * @author root (1 Mar 2022)
+ * 
+ * @param p_core 
+ * 
+ * @return int not used
+ */
+static int f_setLlc(core_t   *p_core){
     char s_path[128]; 
     FILE *fp;
     size_t size;
     char *s_line;
     int n, m;
-//    char s_file[128];
     DIR *p_dir;
     struct dirent *p_dirEntry;
 
@@ -255,13 +292,10 @@ int f_setLlc(core_t   *p_core){
     if (p_dir != NULL) {
         m = 0;
         while ((p_dirEntry = readdir(p_dir))) {
-            //printf("nodes %d %s\n", i_nodes, p_nodeDir->d_name);
             if (!strncmp(p_dirEntry->d_name, "index", 5)) {
-                //found node
                 n = atoi(&p_dirEntry->d_name[5]);
-                printf("\tllc %s %d\n", p_dirEntry->d_name, n);
                 if (n > m) {
-                    m = n;
+                    m = n;  //m track max cache index, depth
                 }
             }
         }
@@ -276,14 +310,15 @@ int f_setLlc(core_t   *p_core){
         }
         size = 0;
         n = getline(&s_line, &size, fp);  
-        m = atoi(s_line);
+        m = atoi(s_line);                   //m now contains llc global id
         printf("\t llc id %d\n", m);
         fclose(fp);
         if (s_line) free(s_line);
 
-        p_llc = f_getLlc(m);
+        p_llc = f_getLlc(m);  //see if llc instance already exits
 
         if (p_llc == NULL) {
+            //new llc required
             p_llc = (llc_t *) malloc(sizeof(llc_t));
             C_topology.i_llcs++;
             if (C_topology.p_llc_head == NULL) {
@@ -296,37 +331,40 @@ int f_setLlc(core_t   *p_core){
             p_llc->p_next = NULL;
             p_llc->i_gIndex = m;
             sprintf(p_llc->c_name, "LLC%d", m);
-            f_setNode(p_core, p_llc);
+            f_setNode(p_core, p_llc);  // add node if required and create linkages
         }
-        p_core->p_llc = p_llc;
-        p_llc->vp_cores[p_llc->i_core_n] = p_core;
+        p_core->p_llc = p_llc;          //associate core with this llc
+        //since syfs is parsed in order the following works
+        p_llc->vp_cores[p_llc->i_core_n] = p_core;  //add llc to core back link
         p_llc->i_core_n++;
-
     }
-
 
     return 0;
 }
 
-int f_getCoreData(int i_core)
+
+/**
+ * @brief using core index add per core tolopogy data
+ * 
+ * @author root (1 Mar 2022)
+ * 
+ * @param i_core 
+ * 
+ * @return int 
+ */
+static int f_getCoreData(int i_core)
 {
     int rc = 0;
-
     char s_path[128]; 
     FILE *fp;
     size_t size;
     char *s_line;
     int n, m;
-//    char s_file[128];
-    //DIR *p_dir;
-    //struct dirent *p_dirEntry;
-
-    //node_t   *p_node;
-    //llc_t    *p_llc;
     core_t   *p_core;
+    int i_smts[8];          //assume max smt of 8 for now
+    int i_smt = 0;
 
 
-    //printf("f_getCoreData(%d)\n", i_core);
     sprintf(s_path, "/sys/devices/system/cpu/cpu%d/topology/thread_siblings_list", i_core);
     fp = fopen(s_path,"r");
     if (fp == NULL) {
@@ -334,156 +372,44 @@ int f_getCoreData(int i_core)
         return -1;
     }
     size = 0;
-    n = getline(&s_line, &size, fp);  
+    n = getline(&s_line, &size, fp); 
+    //line msy be a list of 1 or more core indexs (SMT 2,4,8)
+    //split line string 
+    i_smts[i_smt] = 0;
+    i_smt++;
+
     for (m = 0; m < n; m++) {
         if (s_line[m] ==',') {
             s_line[m] = 0;
-            break;
+            i_smts[i_smt] = m+1;
+            i_smt++;
         }
     }
-    if (m < n) {
-        C_topology.i_smt++;
-        m = atoi(&s_line[m+1]);
-    }
-    else {
-        m = -2;
-    }
-    n = atoi(s_line);
 
-
-    if (i_core == n) {
-        //first or only
-        p_core = (core_t   *) malloc(sizeof(core_t));
-        if (C_topology.p_core_head == NULL) {
-            C_topology.p_core_head = p_core;
-        }
-        else {
-            C_topology.p_core_tail->p_next = p_core;
-        }
-        C_topology.p_core_tail = p_core;
-        p_core->p_next = NULL;
-        p_core->i_gIndex = i_core;
-        sprintf(p_core->c_name, "core%d", i_core);
-        p_core->i_smtIndex = 0;
-
-        f_setLlc(p_core);
-
-    }
-    if (m > 0) {
-        //peer or duplicate
-        p_core = f_getPCore(m);
+    // add all cores the first time through
+    // need smt order
+    for (m = 0; m < i_smt; m++) {
+        n = atoi(&s_line[i_smts[m]]);   //get core index
+        p_core = f_getPCore(n);         //see if it already exits
         if (p_core == NULL) {
             p_core = (core_t   *) malloc(sizeof(core_t));
-            C_topology.p_core_tail->p_next = p_core;
-
+            if (C_topology.p_core_head == NULL) {
+                C_topology.p_core_head = p_core;
+            }
+            else {
+                C_topology.p_core_tail->p_next = p_core;
+            }
             C_topology.p_core_tail = p_core;
             p_core->p_next = NULL;
-            p_core->i_gIndex = m;
-            sprintf(p_core->c_name, "core%d", m);
-            p_core->i_smtIndex = 1;
-
-
+            p_core->i_gIndex = n;
+            sprintf(p_core->c_name, "core%d", n);
+            p_core->i_smtIndex = m;
             f_setLlc(p_core);
         }
     }
+
     fclose(fp);
     if (s_line) free(s_line);
-
-
-
-
-
-
-/*
-    sprintf(s_path, "/sys/devices/system/cpu/cpu%d", i_core);
-    //get parent node
-    p_dir = opendir(s_path);
-    if (p_dir != NULL) {
-        while ((p_dirEntry = readdir(p_dir))) {
-            //printf("nodes %d %s\n", i_nodes, p_nodeDir->d_name);
-            if (!strncmp(p_dirEntry->d_name, "node", 4)) {
-                //found node
-                n = atoi(&p_dirEntry->d_name[4]);
-                printf("\tnode %s %d\n", p_dirEntry->d_name, n);
-                p_node = &C_topology.nodes[n];
-               if (C_topology.i_nodes == 0) {
-                    //C_topology.p_node_head = p_node;
-                    C_topology.i_nodes = n +1;
-                    //p_node->p_next = NULL;
-                    //fill in node
-                    p_node->i_gIndex = n;
-                    strcpy(p_node->c_name, p_dirEntry->d_name);
-                    p_node->i_llc_n = 0;
-                }
-                else if(n ==  C_topology.i_nodes){
-                    C_topology.nodes[n -1].p_next = p_node;
-                    C_topology.i_nodes = n +1;
-                    //p_node->p_next = NULL;
-                    //fill in node
-                    p_node->i_gIndex = n;
-                    strcpy(p_node->c_name, p_dirEntry->d_name);
-                    p_node->i_llc_n = 0;
-                }
-                //else p_node already started
-            }
-        }
-        closedir(p_dir);
-    }
-    //get llc index level (usually 3)
-        //now get core info
-        sprintf(s_path, "/sys/devices/system/cpu/cpu%d/topology/thread_siblings_list", i_core);
-        fp = fopen(s_path,"r");
-        if (fp == NULL) {
-            printf("error %s\n", s_path);
-            return -1;
-        }
-        size = 0;
-        n = getline(&s_line, &size, fp);  
-        printf("\t siblings %s\n", s_line);
-        for (m = 0; m < n; m++) {
-            if (s_line[m] ==',') {
-                s_line[m] = 0;
-                break;
-            }
-        }
-        n = atoi(s_line);
-        if (m < n) {
-            C_topology.i_smt++;
-            m = atoi(&s_line[m+1]);
-        }
-        else {
-            m = -1;
-        }
-
-        if (p_llc->i_core_n == 0) {
-            //first core on this llc
-            //also first parse
-            C_topology.i_cores++;
-            p_llc->i_core_n++
-            p_core = &p_llc->core_list[0];
-            p_core->i_gIndex = i_core;
-            sprintf(p_core->c_name, "core%d", i_core);
-            p_core->i_smtIndex = 0;
-            if (m > 0) {
-                C_topology.i_cores++;
-                p_llc->i_core_n++
-                p_core = &p_llc->core_list[1];
-                p_core->i_gIndex = m;
-                sprintf(p_core->c_name, "core%d", m);
-                p_core->i_smtIndex = 1;
-            }
-        }
-        else if( ){
-
-        }
-
-
-        fclose(fp);
-        if (s_line) free(s_line);
-    }
-
-
-*/
 
 
     return rc;
